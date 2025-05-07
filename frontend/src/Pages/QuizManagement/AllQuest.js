344231149a456d797bed749223e29cf0604ffe96
Devram-ts <159223264+Devram-ts@users.noreply.github.com>
2025-05-07 13:18:59 +0530

    Update AllQuest.js
    Added fetch quiz.

--- frontend/src/Pages/QuizManagement/AllQuest.js
+++ frontend/src/Pages/QuizManagement/AllQuest.js
@@ -7,6 +7,19 @@ function AllQuest() {
     const [quizzes, setQuizzes] = useState([]);
     const navigate = useNavigate();
 
+     useEffect(() => {
+            const fetchQuizzes = async () => {
+                try {
+                    const response = await fetch('http://localhost:8080/quizzes');
+                    const data = await response.json();
+                    setQuizzes(data);
+                } catch (error) {
+                    console.error('Error fetching quizzes:', error);
+                }
+            };
+            fetchQuizzes();
+        }, []);
+
     return (
             <div >
                 <NavBar />
