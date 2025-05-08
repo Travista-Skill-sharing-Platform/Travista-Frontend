9f4841e27168300102fdd5a84f5a5e3d974c09a7
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2025-05-08 17:16:34 +0530

    Define routes in community page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,43 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-      <p>Welcome to your React app.</p>
-    </div>
+    <div>
+      <React.Fragment>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<UserLogin />} />
+          <Route path="/register" element={<UserRegister />} />
+          {/* Protected Routes */}
+          
+          <Route
+            path="/allCommunity"
+            element={
+              <ProtectedRoute>
+                <AllCommunity />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/addNoties/:id"
+            element={
+              <ProtectedRoute>
+                <AddNotices />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/allNoties/:communityId"
+            element={
+              <ProtectedRoute>
+                <AllNotices />
+              </ProtectedRoute>
+            }
+          />
+          
+        </Routes>
+      </React.Fragment>
+      </div>
   );
 }
 
